4facf003d20477fda35d1c4ad760ed181f42b3a9
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-03 18:02:17 -0300

    flex box

--- App.tsx
+++ App.tsx
@@ -3,10 +3,21 @@ import {View, Text, Image, StyleSheet, Button, Alert} from 'react-native';
 
 class App extends React.Component<any, any> {
   render() {
-    return <View style={{flex:1, backgroundColor:'grey'}}>
-      <View style={{flex :1, backgroundColor:'red'}}></View>
-      <View style={{flex :1, backgroundColor:'green'}}></View>
-      <View style={{flex :2, backgroundColor:'yellow'}}></View>
+    return <View style={{
+      flex:1 ,
+      flexDirection:'row', 
+      alignItems:'center',
+     justifyContent:'space-between'
+     }}>
+    {
+    /* <View style={{flex:1 ,flexDirection:'row', justifyContent:'space-between'}}> */}
+      <View style={{width:50, height:50, backgroundColor:'red'}}></View>
+      <View style={{width:50,  height:50,backgroundColor:'green'}}></View>
+      <View style={{width:50,  height:50,backgroundColor:'yellow'}}></View>
+      <View style={{width:50, height:50, backgroundColor:'red'}}></View>
+      <View style={{width:50,  height:50,backgroundColor:'green'}}></View>
+      <View style={{width:50,  height:50,backgroundColor:'yellow'}}></View>
+      
     </View>;
   }
 }
