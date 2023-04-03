bc06f1f280e0d9ba33a6adf63afbb72b17274e19
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-03 20:06:28 -0300

    Text input

--- App.tsx
+++ App.tsx
@@ -1,25 +1,73 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet, Button, Alert} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  Alert,
+  TextInput,
+} from 'react-native';
 
 class App extends React.Component<any, any> {
-  render() {
-    return <View style={{
-      flex:1 ,
-      flexDirection:'row', 
-      alignItems:'center',
-     justifyContent:'space-between'
-     }}>
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      nome: '',
+    };
+
+    this.getNome = this.getNome.bind(this);
+  }
+  getNome(texto: string) {
+    if(texto.length>0)
     {
-    /* <View style={{flex:1 ,flexDirection:'row', justifyContent:'space-between'}}> */}
-      <View style={{width:50, height:50, backgroundColor:'red'}}></View>
-      <View style={{width:50,  height:50,backgroundColor:'green'}}></View>
-      <View style={{width:50,  height:50,backgroundColor:'yellow'}}></View>
-      <View style={{width:50, height:50, backgroundColor:'red'}}></View>
-      <View style={{width:50,  height:50,backgroundColor:'green'}}></View>
-      <View style={{width:50,  height:50,backgroundColor:'yellow'}}></View>
-      
-    </View>;
+      this.setState({nome: 'Bem vindo '+texto});
+    }else{
+      this.setState({nome: ''});
+    }
+
+  }
+
+  render() {
+    let value = '';
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          clearButtonMode="always"
+          keyboardType="default"
+          placeholder="Digite seu nome"
+          underlineColorAndroid="transparent"
+          onChangeText={this.getNome}></TextInput>
+
+        <Text style={styles.texto}>{this.state.nome}</Text>
+      </View>
+    );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    padding: 10,
+    flex: 1,
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  input: {
+    borderRadius: 30,
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 2,
+    fontSize: 18,
+    padding: 10,
+  },
+  texto: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 22,
+  },
+});
 
 export default App;
