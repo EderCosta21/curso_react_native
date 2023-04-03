5338f7ef4ebd07b6335ffc7ef6caec4e573a42a0
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-03 17:47:47 -0300

    Tamanhos dinamicos e fixos

--- App.tsx
+++ App.tsx
@@ -3,32 +3,12 @@ import {View, Text, Image, StyleSheet, Button, Alert} from 'react-native';
 
 class App extends React.Component<any, any> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.titulo}>Eu sou texto 1</Text>
-        <Text style={[styles.titulo, styles.alinharTexto2]}>Eu sou texto 2</Text>
-        <Text style={styles.alinharTexto}>Eu sou texto 3</Text>
-        <Text>Eu sou texto 4</Text>
-        <Text>Eu sou texto 5</Text>
-
-      </View>
-    );
+    return <View style={{flex:1, backgroundColor:'grey'}}>
+      <View style={{flex :1, backgroundColor:'red'}}></View>
+      <View style={{flex :1, backgroundColor:'green'}}></View>
+      <View style={{flex :2, backgroundColor:'yellow'}}></View>
+    </View>;
   }
 }
 
-const styles = StyleSheet.create({
-  container:{
-    marginTop:40,
-  },
-  titulo:{
-    fontSize:25,
-    color:'red'
-  },
-  alinharTexto:{
-    textAlign:'center'
-  },
-  alinharTexto2:{
-    textAlign:'right'
-  }
-})
 export default App;
