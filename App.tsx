f44d5fcba3881fb9afc8cdcb0d3750174496aba2
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-04 13:59:31 -0300

    Flat List

--- App.tsx
+++ App.tsx
@@ -7,68 +7,65 @@ import {
   Button,
   Alert,
   TextInput,
-  ScrollView
+  ScrollView,
+  FlatList,
 } from 'react-native';
 
 class App extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      feed: [
+        {id: '1', nome: 'eder', idade: 5, email: 'eder@gmail.com'},
+        {id: '2', nome: 'eder1', idade: 15, email: 'eder1@gmail.com'},
+        {id: '3', nome: 'eder2', idade: 25, email: 'eder2@gmail.com'},
+        {id: '4', nome: 'eder3', idade: 35, email: 'eder3@gmail.com'},
+        {id: '5', nome: 'eder4', idade: 45, email: 'eder4@gmail.com'},
+      ],
+    };
   }
 
   render() {
-    return <View style={styles.container}>
-      <ScrollView 
-      showsVerticalScrollIndicator={false}
-      horizontal={false}
-      
-      >
-      <View style={styles.box}></View>
-      <View style={styles.box1}></View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
-      <View style={styles.box4}></View>
-      <View style={styles.box5}></View>
-      </ScrollView>
-   
-    </View>;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          keyExtractor={item => item.id}
+          data={this.state.feed}
+          renderItem={({item}) => <Pessoa data={item} />}
+        />
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#c2c2c2',padding:10
+    backgroundColor: '#c2c2c2',
+    padding: 10,
   },
-  box:{
-    backgroundColor:'red',
-    height:150,
-    width:150,
+  textoPessoa: {
+    color: '#fff',
+    fontSize: 22,
   },
-  box1:{
-    backgroundColor:'yellow',
-    height:150,
-    width:150,
+  areaPessoa: {
+    backgroundColor: '#222',
+    height: 200,
+    marginBottom: 15,
   },
-  box2:{
-    backgroundColor:'blue',
-    height:150,
-    width:150,
-  },
-  box3:{
-    backgroundColor:'green',
-    height:150,
-    width:150,
-  },
-  box4:{
-    backgroundColor:'white',
-    height:150,
-    width:150,
-  },
-  box5:{
-    backgroundColor:'black',
-    height:150,width:150,
-  },
-  
 });
 
 export default App;
+
+class Pessoa extends Component<{
+  data: {id: string; nome: string; idade: number; email: string};
+}> {
+  render() {
+    return (
+      <View style={styles.areaPessoa}>
+        <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
+        <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
+        <Text style={styles.textoPessoa}>Email: {this.props.data.email}</Text>
+      </View>
+    );
+  }
+}
