71a44bfc7fb03eb4360063d2aa11b77dd1474ea1
Eder Costa <69824696+EderCosta21@users.noreply.github.com>
2023-04-04 13:34:40 -0300

    projeto default

--- App.tsx
+++ App.tsx
@@ -12,64 +12,18 @@ import {
 class App extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      nome: '',
-      input:''
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-  entrar() {
-  
-      this.setState({nome: 'Bem vindo '+this.state.input});
-  
-
+    this.state = {};
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          clearButtonMode="always"
-          keyboardType="default"
-          placeholder="Digite seu nome"
-          underlineColorAndroid="transparent"
-          onChangeText={(texto)=>this.setState({input:texto})}
-         ></TextInput>
-          
-          <Button color={'red'}  title='Entrar' onPress={()=>this.entrar()}></Button>
-        <Text style={styles.texto}>{this.state.nome}</Text>
-      </View>
-    );
+    return <View style={styles.container}></View>;
   }
 }
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
-    padding: 10,
     flex: 1,
-    borderRadius: 30,
-    borderColor: 'black',
-    borderWidth: 1,
+    backgroundColor:'#c2c2c2'
   },
-  input: {
-    borderRadius: 30,
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#222',
-    margin: 2,
-    fontSize: 18,
-    padding: 10,
-  },
-  texto: {
-    padding: 10,
-    textAlign: 'center',
-    fontSize: 22,
-  },
-  button:{
-    backgroundColor:'red'
-  }
 });
 
 export default App;
